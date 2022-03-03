a4d997774f4b47dcacb5a788075d63a34205e514
huaweiwei <harvey0379@163.com>
2022-03-03 23:43:31 +0800

    feat: send message

--- routes/message.js
+++ routes/message.js
@@ -1,19 +1,72 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const { roster, allLabel, dingURL } = require('../setting');
+const { roster, allLabel, dingURL, accessToken } = require('../setting');
 
 function transform(body) {
-  let bodyString = JSON.stringify(body);
-  roster.forEach((item) => {
-    bodyString = bodyString.replaceAll(`@${item.gitee}`, `@${item.phone}`);
-  });
-  const bodyObject = JSON.parse(bodyString);
-  bodyObject.at = {
-    atMobiles: roster.map((item) => item.phone),
-    isAtAll: allLabel.some((item) => bodyString.includes(item)),
+  const result = {
+    msgtype: 'markdown',
+    password: '',
+    sign: '',
+    access_token: accessToken,
   };
-  return bodyObject;
+
+  const {
+    timestamp,
+    hook_id,
+    hook_name,
+    hook_url,
+    issue,
+    comment,
+    commits,
+    repository,
+    pull_request,
+    sender,
+    action,
+  } = body;
+  result.timestamp = timestamp;
+  result.hook_id = hook_id;
+  result.hook_name = hook_name;
+  result.hook_url = hook_url;
+  let what;
+  switch (hook_name) {
+    case 'push_hooks':
+      what = commits
+        .map(
+          (item) =>
+            `[${item.committer.name}: ${item.message}](${item.url}) [${repository.full_name}](${repository.html_url})`,
+        )
+        .join('\n');
+      break;
+    case 'issue_hooks':
+      what = `[${issue.number} ${issue.title}](${issue.html_url}) \n ${
+        issue.body || ''
+      }`;
+      break;
+    case 'merge_request_hooks':
+      what = `[${pull_request.title}](${pull_request.html_url}) assignee @${pull_request.assignee.username} \n ${pull_request.body}
+      `;
+      break;
+    case 'note_hooks':
+      what = `[${comment.body}](${comment.html_url}) on [${issue.number} ${issue.title}](${issue.html_url})`;
+      break;
+    default:
+      throw 'known hook';
+  }
+  let transformedWhat = what;
+  roster.forEach((item) => {
+    const reg = new RegExp(`@${item.gitee}`, 'g');
+    transformedWhat = transformedWhat.replace(reg, `@${item.phone}`);
+  });
+  result.at = {
+    atMobiles: roster.map((item) => item.phone),
+    isAtAll: allLabel.some((item) => what.includes(item)),
+  };
+  result.markdown = {
+    text: `[${sender.name}](${sender.url}): ${action} ${transformedWhat} `,
+    title: `${hook_name}`,
+  };
+  return result;
 }
 
 router.post('/', (req, res) => {
--- setting.js
+++ setting.js
@@ -1,7 +1,9 @@
 const config = {
   roster: [{ gitee: 'huauauaa', phone: '15311111111' }],
   allLabel: ['@all', '@here'],
-  dingURL: '',
+  dingURL: 'https://oapi.dingtalk.com/robot/send',
+  accessToken:
+    'b536a4269d4ed71341f5529435a9a9b0ff56047529d8a7e2b502e1a003843152',
 };
 
 module.exports = config;
